8f6b47596d1a428cc44f990f2c11bcf6d35ff065
workplacejss <jacobsawyershippee@gmail.com>
2025-02-26 18:21:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-document.querySelector('.dropdown').addEventListener('click', function() {
-    document.querySelector('.dropdown-menu').style.display = 'block';
-});
+function toggleWorks() {
+    let worksList = document.getElementById("works-list");
+    let worksBtn = document.getElementById("works-btn");
+    
+    if (worksList.style.display === "block") {
+        worksList.style.display = "none";
+        worksBtn.classList.remove("clicked");
+    } else {
+        worksList.style.display = "block";
+        worksBtn.classList.add("clicked");
+    }
+}
 
 function showWork(workId) {
     document.querySelectorAll('.work-content').forEach(div => div.style.display = 'none');
